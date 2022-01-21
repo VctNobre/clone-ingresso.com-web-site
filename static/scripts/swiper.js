7762dfcae1de3b49b3665d4d9cabcb02f2de47fb
enzotorrs <enzotorres969@gmail.com>
2022-01-21 15:46:44 -0300

    carrosel e em alta dinâmico finalizado

--- static/scripts/swiper.js
+++ static/scripts/swiper.js
@@ -2,7 +2,10 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
-    slidesPerView: 6,
+    slidesPerView: 'auto',
+    slidesOffsetBefore: 26,
+    spaceBetween: 15,
+    slidesPerGroup: 5,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
