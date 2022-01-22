5e21ac6e938e3fe49efd62a16dc48aba75173eaf
enzotorrs <enzotorres969@gmail.com>
2022-01-22 18:41:47 -0300

    em cartaz e pagina de filmes completamente dinâmica

--- filmes/views.py
+++ filmes/views.py
@@ -3,11 +3,17 @@ import requests
 import json
 
 def index(request):
-    requisisao = requests.get('https://ingressoapi.herokuapp.com/filmes/')
+    filmes_cartaz = requests.get('https://ingressoapi.herokuapp.com/filmes/cartaz/') 
+    filmes_alta = requests.get('https://ingressoapi.herokuapp.com/filmes/alta/') 
     dados = {
-        'filmes': json.loads(requisisao.content)
+        'filmes_cartaz': json.loads(filmes_cartaz.content),
+        'filmes_alta': json.loads(filmes_alta.content),
     }
     return render(request, 'index.html', dados)
 
 def filmes(request):
-    return render(request, 'filmes.html')
+    todos_filmes = requests.get('https://ingressoapi.herokuapp.com/filmes/')
+    dados = {
+        'filmes': json.loads(todos_filmes.content)
+    }
+    return render(request, 'filmes.html', dados)
