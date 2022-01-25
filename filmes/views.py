e619aed93db2831fc568fbbe7f36b35090972db6
Vicente Nobre <vicente_nobre@hotmail.com>
2022-01-25 18:39:40 -0300

    fanshop e em breve finalizados/estruturação das promoções

--- filmes/views.py
+++ filmes/views.py
@@ -8,11 +8,15 @@ def index(request):
     filmes_alta = requests.get('https://ingressoapi.herokuapp.com/filmes/alta/') 
     noticias = requests.get('https://ingressoapi.herokuapp.com/noticias/')
     banner = requests.get('https://ingressoapi.herokuapp.com/filmes/principal/') 
+    fan_shop = requests.get('https://ingressoapi.herokuapp.com/fanshop/')
+    filmes_em_breve = requests.get('https://ingressoapi.herokuapp.com/filmes/breve/')
     dados = {
         'filmes_cartaz': json.loads(filmes_cartaz.content),
         'filmes_alta': json.loads(filmes_alta.content),
         'noticias':json.loads(noticias.content),
          'banner': json.loads(banner.content)[0],
+         'fan_shop': json.loads(fan_shop.content),
+         'filmes_breve': json.loads(filmes_em_breve.content)[0:10],
     }
     return render(request, 'index.html', dados)
 
